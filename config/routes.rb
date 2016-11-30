b8cc08d28097d06abf06330299b26df4eea01410
Jones Reston <jonesreston@yahoo.com>
2016-11-30 18:03:34 +0000

    create style and ingredients display and user interface

--- config/routes.rb
+++ config/routes.rb
@@ -26,4 +26,6 @@ Rails.application.routes.draw do
   post '/login', to: "logins#create"
   get '/logout', to: "logins#destroy"
   
+  resources :styles, only: [:new, :create, :show]
+  resources :ingredients, only: [:new, :create, :show]
 end
